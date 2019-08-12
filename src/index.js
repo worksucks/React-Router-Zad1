e3ff2640a77ffa05bddeb15c75ed9d6ceb92539e
Tomasz Reizer <t_reizer@yahoo.com>
2019-08-12 16:38:23 +0200

    ZAD2 - Problem described in communicator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { render } from 'react-dom';
+import {Router, Route, hashHistory } from 'react-router';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from './DevTools';
+import Navigation from './presentational/navigation.component';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <div>
+      <Router history={hashHistory} routes={routes}>
+        <h1>Inicjalizacja projektu</h1>
+        <DevTools/>
+      </Router>
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/presentational/home.component.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Home = () => (
+  <h1> Witamy w aplikacji państwa.js</h1>
+);
+
+export default Home;
